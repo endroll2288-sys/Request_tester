12d2ca3e98bbb0fce1d4ccb2542a054446ffd9ce
endroll2288-sys <endroll2288@gmail.com>
2025-10-24 13:01:07 +0900

    Update script.js

--- toll/script.js
+++ toll/script.js
@@ -2,15 +2,18 @@ document.getElementById('sendRequest').addEventListener('click', async () => {
     const url = document.getElementById('urlInput').value;
     const method = document.getElementById('methodSelect').value;
     const body = document.getElementById('bodyInput').value;
+    const x-csrf-token=document.getElementById("x-csrf-tokenInput").value;
     const token = document.getElementById('tokenInput').value;
 
+    
     try {
         console.log(`${token} and ${body}`)
         const response = await fetch(url, {
             method: method,
             headers: {
                 'Content-Type': 'application/json',
-                 'Authorization': `Bearer ${token}`, 
+                'Authorization': `Bearer ${token}`, 
+                "x-csrf-token":`${x-csrf-token}`
             },
             body: method === 'GET' ? null : body ? JSON.stringify(body) : undefined,
         });
