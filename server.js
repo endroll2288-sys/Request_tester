7749dabc3049442abe2faa9b3f4c552b5a64f3a2
ROLL <endroll2288@gmail.com>
2026-06-29 13:34:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import axios from 'axios';
 import cors from'cors';
 import path from "path";
+import * as cheerio from 'cheerio'; // 1. cheerioをインポート
 //const path = require('path')
 const app = express();
 const PORT = 3000;
